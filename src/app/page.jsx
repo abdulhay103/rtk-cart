f22a97bbc1cbcbc31587b29f39306a79e148fdaf
abdulhay103 <abdulhay.me@gmail.com>
2024-07-26 19:59:57 +0600

    working

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,12 @@ export default function mainPage() {
   return (
     <div className=" flex items-center justify-center min-h-screen">
       <div className=" space-y-10 text-center">
-        <h1 className=" text-blue-600 text-2xl font-black">Welcome to RTK dream Factory!</h1>
-        <Link href={"/products"} className=" text-orange-500 text-lg font-bold">Buy Product</Link>
+        <h1 className=" text-blue-600 text-2xl font-black">
+          Welcome to RTK dream Factory!
+        </h1>
+        <Link href={"/products"} className=" text-orange-500 text-lg font-bold">
+          Buy Product
+        </Link>
       </div>
     </div>
   );
